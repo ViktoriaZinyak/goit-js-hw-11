129d3d09fea2812d5670a9b3634a694996796af5
ViktoriyaZinyak <viktoriazinyak@gmail.com>
2022-09-06 18:46:49 +0300

    notif color

--- src/index.js
+++ src/index.js
@@ -1,131 +1,3 @@
-// import PixabayApi from './js/pixabay-api';
-
-// const form = document.querySelector('.search-form');
-// const buttonLoadMore = document.querySelector('.load-more');
-// const photosWrap = document.querySelector('.gallery');
-
-// const pixabayApi = new PixabayApi();
-// buttonLoadMore.classList.add('is-hidden');
-// form.addEventListener('submit', onSearch);
-// buttonLoadMore.addEventListener('click', onLoadMore);
-
-// function onSearch(e) {
-//   e.preventDefault();
-//   pixabayApi.query = e.currentTarget.elements.searchQuery.value.trim();
-//   pixabayApi.resetPage();
-//   pixabayApi
-//     .fetchPhotos()
-//     .then(data => data.hits)
-//     .then(photos => {
-//       clearPhotosWrap();
-//       pixabayApi.notification().then(data => {
-//         if (data > 0) {
-//           buttonLoadMore.classList.remove('is-hidden');
-//         }
-//       });
-//       photosWrap.insertAdjacentHTML('beforeend', photoTemplate(photos));
-//     });
-//   buttonLoadMore.classList.add('is-hidden');
-// }
-
-// function onLoadMore() {
-//   pixabayApi
-//     .fetchPhotos()
-//     .then(data => data.hits)
-//     .then(appendPhotosMarkup);
-// }
-
-// function photoTemplate(photos) {
-//   return photos
-//     .map(
-//       el => `<div class="photo-card">
-//   <img src="${el.webformatURL}" alt="${el.tags}" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes ${el.likes}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views ${el.views}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments ${el.comments}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads ${el.downloads}</b>
-//     </p>
-//   </div>
-// </div>`
-//     )
-//     .join('');
-// }
-
-// function appendPhotosMarkup(photos) {
-//   photosWrap.insertAdjacentHTML('beforeend', photoTemplate(photos));
-// }
-
-// function clearPhotosWrap() {
-//   photosWrap.innerHTML = '';
-// }
-
-// import PixabayApi from './js/pixabay-api';
-
-// const form = document.querySelector('.search-form');
-// const buttonLoadMore = document.querySelector('.load-more');
-// const photosWrap = document.querySelector('.gallery');
-
-// const pixabayApi = new PixabayApi();
-// buttonLoadMore.classList.add('is-hidden');
-// form.addEventListener('submit', onSearch);
-// buttonLoadMore.addEventListener('click', onLoadMore);
-
-// function onSearch(e) {
-//   e.preventDefault();
-//   pixabayApi.query = e.currentTarget.elements.searchQuery.value.trim();
-//   pixabayApi.resetPage();
-//   pixabayApi.fetchPhotos().then(photos => {
-//     clearPhotosWrap();
-//     pixabayApi.notification();
-//     photosWrap.insertAdjacentHTML('beforeend', photoTemplate(photos));
-//     buttonLoadMore.classList.remove('is-hidden');
-//   });
-// }
-
-// function onLoadMore() {
-//   pixabayApi.fetchPhotos().then(appendPhotosMarkup);
-// }
-
-// function photoTemplate(photos) {
-//   return photos
-//     .map(
-//       el => `<div class="photo-card">
-//   <img src="${el.webformatURL}" alt="${el.tags}" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes ${el.likes}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views ${el.views}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments ${el.comments}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads ${el.downloads}</b>
-//     </p>
-//   </div>
-// </div>`
-//     )
-//     .join('');
-// }
-
-// function appendPhotosMarkup(photos) {
-//   photosWrap.insertAdjacentHTML('beforeend', photoTemplate(photos));
-// }
-
-// function clearPhotosWrap() {
-//   photosWrap.innerHTML = '';
-// }
-
 import PixabayApi from './js/pixabay-api';
 import Notiflix from 'notiflix';
 
@@ -146,8 +18,11 @@ async function onSearch(e) {
   const data = await pixabayApi.fetchPhotos();
   buttonLoadMore.classList.add('is-hidden');
   if (data.totalHits > 0) {
-    Notiflix.Notify.warning(`Hooray! We found ${data.totalHits} images.`);
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
     buttonLoadMore.classList.remove('is-hidden');
+  } else {
+    Notiflix.Notify.failure();
+    `Sorry, there are no images matching your search query. Please try again.`;
   }
   appendPhotosMarkup(data.hits);
 }
@@ -159,8 +34,6 @@ async function onLoadMore() {
   }
   appendPhotosMarkup(data.hits);
 }
-// .then(appendPhotosMarkup);
-//   pixabayApi.fetchPhotos().then(appendPhotosMarkup);
 
 function photoTemplate(photos) {
   if (photos.length < 40 && photos.length != 0) {
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,100 +1,5 @@
-// import axios from 'axios';
-// const KEY = '29652789-9575d4159aae8e630a1d59f50';
-// import Notiflix from 'notiflix';
-
-// export default class PixabayApi {
-//   constructor() {
-//     this.searchQuery = '';
-//     this.page = 1;
-//   }
-
-//   fetchPhotos() {
-//     const api = axios.create({
-//       baseURL: 'https://pixabay.com/api/',
-//     });
-//     return api
-//       .get(
-//         `?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`
-//       )
-//       .then(data => {
-//         this.page += 1;
-//         return data.data;
-//       });
-//   }
-
-//   resetPage() {
-//     this.page = 1;
-//   }
-
-//   get query() {
-//     return this.searchQuery;
-//   }
-
-//   set query(newQuery) {
-//     this.searchQuery = newQuery;
-//   }
-
-//   notification() {
-//     return this.fetchPhotos().then(data => {
-//       if (data.totalHits > 0) {
-//         Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-//       } else {
-// Notiflix.Notify.failure('Oops, there is no photo with that name');
-//       }
-//       return data.totalHits;
-//     });
-//   }
-// }
-
-// import axios from 'axios';
-// const KEY = '29652789-9575d4159aae8e630a1d59f50';
-// import Notiflix from 'notiflix';
-
-// export default class PixabayApi {
-//   constructor() {
-//     this.searchQuery = '';
-//     this.page = 1;
-//   }
-
-//   fetchPhotos() {
-//     const api = axios.create({
-//       baseURL: 'https://pixabay.com/api/',
-//     });
-//     return api
-//       .get(
-//         `?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
-//       )
-//       .then(res => {
-//         return res.data;
-//       })
-//       .then(data => {
-//         this.page += 1;
-//         return data.hits;
-//       });
-//   }
-
-//   resetPage() {
-//     this.page = 1;
-//   }
-
-//   get query() {
-//     return this.searchQuery;
-//   }
-
-//   set query(newQuery) {
-//     this.searchQuery = newQuery;
-//   }
-
-//   notification() {
-//     this.fetchPhotos().then(data =>
-//       Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
-//     );
-//   }
-// }
-
 import axios from 'axios';
 const KEY = '29652789-9575d4159aae8e630a1d59f50';
-import Notiflix from 'notiflix';
 
 export default class PixabayApi {
   constructor() {
@@ -106,25 +11,12 @@ export default class PixabayApi {
     const api = axios.create({
       baseURL: 'https://pixabay.com/api/',
     });
-    try {
-      const resposne = await api.get(
-        `?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
-      );
-      const newObj = await resposne.data;
-      this.page += 1;
-      return newObj;
-    } catch (error) {
-      console.log('hjk');
-    }
-    // return api
-    //   .get(
-    //     `?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
-    //   )
-    //   .then(res => {
-    //     this.page += 1;
-    //     return res.data;
-    //   })
-    //   .catch(console.log('hjk'));
+    const resposne = await api.get(
+      `?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
+    );
+    const newObj = await resposne.data;
+    this.page += 1;
+    return newObj;
   }
 
   resetPage() {
@@ -138,10 +30,4 @@ export default class PixabayApi {
   set query(newQuery) {
     this.searchQuery = newQuery;
   }
-
-  // notification() {
-  //   this.fetchPhotos().then(data =>
-  //     Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
-  //   );
-  // }
 }
